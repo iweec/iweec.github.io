32836e6f78303d761cfcf4fbeffe901986728243
iweec <liulichen2011@outlook.com>
2024-05-14 21:55:34 +0800

    Site updated: 2024-05-14 21:55:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/12/hello-world/","2024/05/13/博客更新！欢迎光临！/"];function toRandomPost(){
+var posts=["2024/05/12/hello-world/","2024/05/13/博客更新！欢迎光临！/","2024/05/14/新的Bing镜像！秒过人机验证！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
